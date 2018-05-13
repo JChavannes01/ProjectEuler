8d4a91f3e490d998898967a9aaa405a4ee8ada4f
JChavannes01 <janko.chavannes@gmail.com>
2018-05-13 17:15:05 +0200

    Added up to 17, solved up to 12

--- Euler.py
+++ Euler.py
@@ -122,6 +122,7 @@ def problem7():
     
     print( primes[-1] )
     
+    
 def problem8():
     """ The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 
@@ -236,64 +237,73 @@ def problem11():
 
         The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
         What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?"""
-    grid = []
-    grid.append([08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08]) 
-    grid.append([49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00]) 
-    grid.append([81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65]) 
-    grid.append([52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91])
+    grid = list()
+    grid.append([ 8,  2, 22, 97, 38, 15, 00, 40, 00, 75,  4,  5,  7, 78, 52, 12, 50, 77, 91,  8]) 
+    grid.append([49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48,  4, 56, 62, 00]) 
+    grid.append([81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30,  3, 49, 13, 36, 65]) 
+    grid.append([52, 70, 95, 23,  4, 60, 11, 42, 69, 24, 68, 56,  1, 32, 56, 71, 37,  2, 36, 91])
     grid.append([22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80])
-    grid.append([24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50])
+    grid.append([24, 47, 32, 60, 99,  3, 45,  2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50])
     grid.append([32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70])
-    grid.append([67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21])
-    grid.append([24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72])
-    grid.append([21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95])
-    grid.append([78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92])
-    grid.append([16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57])
-    grid.append([86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58])
-    grid.append([19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40])
-    grid.append([04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66])
-    grid.append([88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69])
-    grid.append([04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36])
-    grid.append([20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16])
-    grid.append([20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54])
-    grid.append([01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48])        
-    # grid(row, col)
+    grid.append([67, 26, 20, 68,  2, 62, 12, 20, 95, 63, 94, 39, 63,  8, 40, 91, 66, 49, 94, 21])
+    grid.append([24, 55, 58,  5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72])
+    grid.append([21, 36, 23,  9, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95])
+    grid.append([78, 17, 53, 28, 22, 75, 31, 67, 15, 94,  3, 80,  4, 62, 16, 14,  9, 53, 56, 92])
+    grid.append([16, 39,  5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57])
+    grid.append([86, 56, 00, 48, 35, 71, 89,  7,  5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58])
+    grid.append([19, 80, 81, 68,  5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77,  4, 89, 55, 40])
+    grid.append([ 4, 52,  8, 83, 97, 35, 99, 16,  7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66])
+    grid.append([88, 36, 68, 87, 57, 62, 20, 72,  3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69])
+    grid.append([ 4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18,  8, 46, 29, 32, 40, 62, 76, 36])
+    grid.append([20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74,  4, 36, 16])
+    grid.append([20, 73, 35, 29, 78, 31, 90,  1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57,  5, 54])
+    grid.append([ 1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52,  1, 89, 19, 67, 48])        
+    # grid[row][col]
     
     largestProduct = 0
+    originRow, originCol, position = 0, 0, ""
     
     for row in range(len(grid)):
-        for col in range(len(row)):
+        for col in range(len(grid[row])):
             # check vertical
             if row+3 < len(grid):
-                largestProduct = max(largestProduct, product([ grid[row][col+offset] for offset in range(4)])) 
+                if product([ grid[row+offset][col] for offset in range(4)]) > largestProduct:
+                    originRow, originCol, position = row, col, "Vertical"
+                largestProduct = max(largestProduct, product([ grid[row+offset][col] for offset in range(4)])) 
                 
             # check horizontal
-            if col+3 < len(row):
-                largestProduct = max(largestProduct, product([ grid[row+offset][col] for offset in range(4)]))            
+            if col+3 < len(grid[row]):
+#                if product([ grid[row][col+offset] for offset in range(4)]) > largestProduct:
+#                        originRow, originCol, position = row, col, "Horizontal"
+                largestProduct = max(largestProduct, product([ grid[row][col+offset] for offset in range(4)]))            
             
             # check diagonal bottom right
-            if row+3 < len(grid) and col+3 < len(row):
+            if row+3 < len(grid) and col+3 < len(grid[row]):
+#                if product([ grid[row+offset][col+offset] for offset in range(4)]) > largestProduct:
+#                        originRow, originCol, position = row, col, "Diagonal \\"
                 largestProduct = max(largestProduct, product([ grid[row+offset][col+offset] for offset in range(4)]))
             
             # check diagonal bottom left
             if row+3 < len(grid) and col > 3:
-                largestProduct = max(largestProduct, product([ grid[row-offset][col+offset] for offset in range(4)]))
+#                if product([ grid[row+offset][col-offset] for offset in range(4)]) > largestProduct:
+#                        originRow, originCol, position = row, col, "Diagonal /"
+                largestProduct = max(largestProduct, product([ grid[row+offset][col-offset] for offset in range(4)]))
                 
-    print("problem11: largestProduct =", largestProduct)
+    print("problem11: largestProduct = {}, at {},{} {}".format(largestProduct, originRow, originCol, position))
+    
     
 def product(list):
-    product = 1
+    """Helper for problem 11, returns the product of a list of numbers"""
+    product = 1 if len(list) > 0 else 0
     for x in list:
         product *= x
     return product
     
+    
 def problem12():
     """ The sequence of triangle numbers is generated by adding the natural numbers. So the 7th triangle number would be 1 + 2 + 3 + 4 + 5 + 6 + 7 = 28. The first ten terms would be:
-
             1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
-
-        Let us list the factors of the first seven triangle numbers:
-
+       Let us list the factors of the first seven triangle numbers:
              1: 1
              3: 1,3
              6: 1,2,3,6
@@ -302,8 +312,28 @@ def problem12():
             21: 1,3,7,21
             28: 1,2,4,7,14,28
         We can see that 28 is the first triangle number to have over five divisors.
-
         What is the value of the first triangle number to have over five hundred divisors?"""
+    triN, i = 0, 1
+    while True:
+        numOfDivs = 1
+        triN += i
+        i += 1
+        for exp in factorise(triN).values():
+            numOfDivs *= exp+1
+        if numOfDivs > 500:
+            print(triN, numOfDivs)
+            break
+        
+
+def factorise(n):
+    p, factorisation = 2, dict()
+    while p <= n:
+        if n % p == 0:
+            n = n // p
+            factorisation[p] = factorisation.get(p,0) + 1
+        else:
+            p += 1
+    return factorisation
         
         
 def problem13():
@@ -423,7 +453,21 @@ def problem14():
         NOTE: Once the chain starts the terms are allowed to go above one million."""
         
 
-
-
+def problem15():
+    """ Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+        How many such routes are there through a 20×20 grid?"""
+    
+    
+def problem16():
+    """ 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+        What is the sum of the digits of the number 21000?"""
+    
+    
+def problem17():
+    """ If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+        If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+        NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage."""
+        
+        
 if __name__ == '__main__':
-    problem11()
+    problem12()
