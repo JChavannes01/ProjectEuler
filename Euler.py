ca315dcc7055174f7fca56960386c8d1967d78b0
JChavannes01 <janko.chavannes@gmail.com>
2018-05-21 17:39:09 +0200

    Added up to problem 27

--- Euler.py
+++ Euler.py
@@ -577,5 +577,81 @@ def alphabeticalValue(name):
     offset = ord('a')-1
     return sum( [ord(letter)-offset for letter in name.lower()] )
     
+    
+def problem23():
+    """A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+    A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+    As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+    Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers."""
+    
+    
+def problem24():
+    """ A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+        012   021   102   120   201   210
+
+    What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?"""
+    
+    
+def problem25():
+    """ The Fibonacci sequence is defined by the recurrence relation:
+
+    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+    Hence the first 12 terms will be:
+
+        F1 = 1
+        F2 = 1
+        F3 = 2
+        F4 = 3
+        F5 = 5
+        F6 = 8
+        F7 = 13
+        F8 = 21
+        F9 = 34
+        F10 = 55
+        F11 = 89
+        F12 = 144
+    The 12th term, F12, is the first term to contain three digits.
+
+    What is the index of the first term in the Fibonacci sequence to contain 1000 digits?"""
+    
+    
+def problem26():
+    """ A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+
+        1/2	= 	0.5
+        1/3	= 	0.(3)
+        1/4	= 	0.25
+        1/5	= 	0.2
+        1/6	= 	0.1(6)
+        1/7	= 	0.(142857)
+        1/8	= 	0.125
+        1/9	= 	0.(1)
+        1/10	= 	0.1
+    Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+
+    Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part."""
+    
+    
+def problem27():
+    """ Euler discovered the remarkable quadratic formula:
+
+    n^2+n+41
+    It turns out that the formula will produce 40 primes for the consecutive integer values 0≤n≤39. However, when n=40,40^2+40+41=40(40+1)+41 is divisible by 41, and certainly when n=41,41^2+41+41 is clearly divisible by 41.
+
+    The incredible formula n^2−79n+1601 was discovered, which produces 80 primes for the consecutive values 0≤n≤79. The product of the coefficients, −79 and 1601, is −126479.
+
+    Considering quadratics of the form:
+
+    n^2+an+b, where |a|<1000 and |b|≤1000
+
+    where |n| is the modulus/absolute value of n
+    e.g. |11|=11 and |−4|=4
+    Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0."""
+    
+    
 if __name__ == '__main__':
     problem19()
